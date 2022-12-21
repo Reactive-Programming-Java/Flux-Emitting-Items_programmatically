c4b8871385be2ad3fa8221ec5f002f4f0e1ee539
efofana <elimane.fofana@ext.soprasteria.com>
2022-12-21 17:01:12 +0100

    assignment done

--- src/main/java/assignment/solution/FileReaderServiceTest.java
+++ src/main/java/assignment/solution/FileReaderServiceTest.java
@@ -4,13 +4,18 @@ import Util.Util;
 import assignment.resolution.FileReader;
 
 import java.nio.file.Path;
+import java.util.Locale;
+import java.util.ResourceBundle;
 
 /**
  * @author efofana
  */
 public class FileReaderServiceTest {
+
     public static void main(String[] args) {
         Path path = Util.FILEPATH;
-        FileReaderService.read(path).subscribe(Util.subscriber());
+        FileReaderService.read(path)
+                .take(4) // To read only 4 lines
+                .subscribe(Util.subscriber());
     }
 }
